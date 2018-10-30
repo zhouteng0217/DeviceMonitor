9cc78d18e97b27594a7d23de20b74ae9e14e23c9
zhouteng <zhouteng@cmstop.com>
2018-10-31 01:07:25 +0800

    启动AndroidDeviceMonitor

---
+++ src/com/zt/DeviceMonitor.java
@@ -0,0 +1,29 @@
+package com.zt;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.util.EnvironmentUtil;
+
+public class DeviceMonitor extends AnAction {
+
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        String osName = System.getProperty("os.name");
+        String monitorPath = EnvironmentUtil.getValue("ANDROID_HOME");
+
+        if (osName.toLowerCase().startsWith("win")) {
+            monitorPath  += "\\tools\\monitor.bat";
+        } else {
+            monitorPath += "/tools/monitor";
+        }
+
+        try {
+            Runtime.getRuntime().exec(monitorPath);
+        } catch (Exception exception) {
+
+        }
+    }
+
+
+}
